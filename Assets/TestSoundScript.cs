961bf4352c80cd018dad0dca8d09542a97756192
stebssbets <stebssbets@yahoo.com>
2019-05-25 20:35:37 -0400

    Script and Studio listener
    Adding a c# script and studio listener adds 3 files.

---
+++ Assets/TestSoundScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSoundScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/TestSoundEvent");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
